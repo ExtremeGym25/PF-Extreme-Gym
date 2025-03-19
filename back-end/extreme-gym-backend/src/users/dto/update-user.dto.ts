654a11b3bdd934162f72778a2bb1cf9a512c2c54
Sebaswolf31 <sebasunivercal@hotmail.com>
2025-03-19 18:00:21 -0500

    creacion DTO update y la carpeta helpers para el match de password

--- back-end/extreme-gym-backend/src/users/dto/update-user.dto.ts
+++ back-end/extreme-gym-backend/src/users/dto/update-user.dto.ts
@@ -1,4 +1,67 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MinLength,
+  MaxLength,
+  Matches,
+  IsNumber,
+  Validate,
+  IsOptional,
+} from 'class-validator';
+import { MatchPassword } from '../../helpers/matchPassword';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$^&*])/, {
+    message:
+      'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.',
+  })
+  @MinLength(8)
+  @MaxLength(15)
+  password?: string;
+
+  @IsOptional()
+  @Validate(MatchPassword, ['password'])
+  confirmPassword?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(80)
+  address?: string;
+
+  @IsOptional()
+  @IsNumber()
+  phone?: number;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(5)
+  @MaxLength(20)
+  country?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(5)
+  @MaxLength(20)
+  city?: string;
+
+  @IsOptional()
+  @IsString()
+  subcriptionType?: string;
+
+  @IsOptional()
+  @IsString()
+  profileImage?: string;
+}
