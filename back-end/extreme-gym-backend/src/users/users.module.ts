7950515b58cc9f39db974aaab617e3c444898b21
Sebaswolf31 <sebasunivercal@hotmail.com>
2025-03-19 18:51:43 -0500

    terminada la configuracion del module de usuarios

--- back-end/extreme-gym-backend/src/users/users.module.ts
+++ back-end/extreme-gym-backend/src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { AuthService } from '../auth/auth.service';
 
 @Module({
-  controllers: [UsersController],
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController, AuthService],
   providers: [UsersService],
 })
 export class UsersModule {}
