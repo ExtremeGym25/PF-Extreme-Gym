fd40d8ac269e51a0135c172b46c8798dbbed8d5a
Halortiz73 <halortiz73@gmail.com>
2025-03-18 18:49:16 -0600

    added DB connection

---
+++ back-end/extreme-gym-backend/src/config/typeorm.ts
@@ -0,0 +1,18 @@
+import {config as dotenvConfig} from 'dotenv'
+import { registerAs } from '@nestjs/config'
+
+dotenvConfig({path: '.env'})
+const config = {
+    type: 'postgres',
+    host : `${process.env.DB_HOST}` || 'localhost',
+    username : `${process.env.DB_USER}`,
+    password: `${process.env.DB_PASSWORD}`,
+    database: `${process.env.DB_NAME}`,
+    port: process.env.DB_PORT,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    autoLoadEntities: true,
+    dropSchema: true,
+    synchronize: true
+}
+
+export default registerAs('typeorm', () => config)
