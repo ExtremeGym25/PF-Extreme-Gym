2585329c8973cd8264c3e235c9d7c1223790b225
pandreasantacruz <andreasantacruz123@gmail.com>
2025-03-22 12:49:02 -0500

    View mi perfil

--- front-end/src/app/miPerfil/page.tsx
+++ front-end/src/app/miPerfil/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import usePrivate from "../hooks/usePrivate";
+import Acordeon from "../components/viewcomponents/acordeon";
 
 const MiPerfil = () => {
   const loading = usePrivate();
@@ -14,17 +15,36 @@ const MiPerfil = () => {
   }
 
   return (
-    <div className="py-4 bg-background font-poppins">
-      <div>
-        <h2 className="py-2 text-3xl font-bold text-center transition-transform duration-300 hover:scale-110">
-          Mi Perfil
-        </h2>
-      </div>
+    <div className="flex flex-col items-center min-h-screen bg-background font-poppins">
+      <h2 className="py-8 text-4xl font-extrabold text-center text-foreground">
+        Mi Perfil
+      </h2>
+      <div className="grid w-full max-w-6xl grid-cols-1 md:grid-cols-2">
+        <div className="flex flex-col items-center justify-center ">
+          <div className="w-full max-w-xl p-6 space-y-4 shadow-2xl bg-fondo rounded-2xl">
+            <p className="text-lg font-semibold text-center text-foreground">
+              Información del usuario.
+            </p>
 
-      <div className="p-4 mx-auto transition-transform duration-300 rounded-lg shadow-md bg-blackP w-96 hover:scale-110">
-        <p className="text-center text-l">
-          Información del usuario aún no disponible.
-        </p>
+            <Acordeon title="Información Personal">
+              <p>Aquí va la información del usuario.</p>
+            </Acordeon>
+
+            <Acordeon title="Compras Realizadas">
+              <p>Aquí va la información de compras.</p>
+            </Acordeon>
+
+            <Acordeon title="Eventos">
+              <p>Aquí va la información de eventos.</p>
+            </Acordeon>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center w-full max-w-xl p-6 space-y-4 shadow-2xl bg-fondo rounded-2xl">
+          <p className="text-lg font-semibold text-foreground">
+            Información adicional del usuario.foto
+          </p>
+        </div>
       </div>
     </div>
   );
