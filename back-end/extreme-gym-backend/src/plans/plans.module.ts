ebccfa5014428ca8ba660e929d19dafa3e8174a9
Sebaswolf31 <sebasunivercal@hotmail.com>
2025-03-26 21:48:43 -0500

    modificciones en el plans.module.ts

--- back-end/extreme-gym-backend/src/plans/plans.module.ts
+++ back-end/extreme-gym-backend/src/plans/plans.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PlanService } from './plans.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PlanController } from './plans.controller';
+import { PlanService } from './plans.service';
+import { Plan } from './entities/plan.entity';
+import { UserPlan } from './entities/user-plan.entity';
+import { User } from '../users/entities/user.entity';
+import { Notification } from '../notifications/entities/notification.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Plan, UserPlan, User, Notification])],
   controllers: [PlanController],
   providers: [PlanService],
 })
