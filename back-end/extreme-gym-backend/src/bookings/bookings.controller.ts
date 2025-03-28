d684e8e35fe950f00fafdc8cef7017b567fa753d
JFernandezBalza <joss.jota21@gmail.com>
2025-03-28 20:56:33 -0300

    implementacion de auth en todos los controladores

--- back-end/extreme-gym-backend/src/bookings/bookings.controller.ts
+++ back-end/extreme-gym-backend/src/bookings/bookings.controller.ts
@@ -8,23 +8,34 @@ import {
   Delete,
   HttpException,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { CreateBookingDto } from './dto/create-booking.dto';
 import { UpdateBookingDto } from './dto/update-booking.dto';
 import { Booking } from './entities/booking.entity';
+import { AuthGuard } from 'src/auth/guards/auth.guard';
+import { User as UserDecorator } from '../decorators/user.decorator';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/decorators/roles.decorators';
+import { Role } from 'src/users/entities/roles.enum';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('bookings')
+@UseGuards(AuthGuard)
 export class BookingsController {
   constructor(private readonly bookingsService: BookingsService) {}
 
-  // Obtener todas las reservas
   @Get()
+  @UseGuards(RolesGuard)
+  @Roles(Role.Admin)
   async findAll(): Promise<Booking[]> {
     return await this.bookingsService.findAllBookings();
   }
 
   @Get(':id')
+  @UseGuards(RolesGuard)
+  @Roles(Role.Admin)
   async findOne(@Param('id') id: string): Promise<Booking> {
     try {
       return await this.bookingsService.findBookingById(id);
@@ -34,6 +45,11 @@ export class BookingsController {
         error.getStatus ? error.getStatus() : HttpStatus.NOT_FOUND,
       );
     }
+  }
+
+  @Get('my-reservations')
+  async findMyReservations(@UserDecorator() user: User): Promise<Booking[]> {
+    return await this.bookingsService.findBookingsByUserId(user.id);
   }
 
   @Post()
